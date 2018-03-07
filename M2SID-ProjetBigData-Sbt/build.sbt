ae7c4236ba3644322e1c1aebb4fe2e642c28f673
idSaid <id.said@live.fr>
2018-03-07 01:26:52 +0100

    Q3 kmeans

--- M2SID-ProjetBigData-Sbt/build.sbt
+++ M2SID-ProjetBigData-Sbt/build.sbt
@@ -5,4 +5,8 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+
 libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.0.2"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.0.2"
+
+
---
+++ M2SID-ProjetBigData-Sbt/src/main/scala-2.11/Q3.scala
@@ -0,0 +1,53 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.clustering.KMeans
+
+import scala.util.Try
+
+
+/**
+  * Created by Said on 06/03/2018.
+  */
+object Q3 {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Prog1").setMaster("local[*]")
+    val sc: SparkContext = new SparkContext(conf)
+
+    val data = sc.textFile("C:\\Users\\Said\\IdeaProjects\\untitled\\M2SID-ProjetBigData-Sbt\\extraitCrimes.csv", 2)
+    val header = data.first()
+    val filterdData = data.filter(row => row != header)
+
+    val latLong = filterdData
+      .filter(l => {
+        val line = l.split(",")
+        val lattitude = line(line.length - 4)
+        val longitude = line(line.length - 3)
+        lattitude.nonEmpty && longitude.nonEmpty && Try(lattitude.toDouble).isSuccess && Try(longitude.toDouble).isSuccess
+      })
+      .map(l => {
+        val line = l.split(",")
+        val lattitude = line(line.length - 4)
+        val longitude = line(line.length - 3)
+        (lattitude.toDouble, longitude.toDouble)
+      })
+
+    val vectors = latLong.map(ll => Vectors.dense( ll._1,ll._2)).cache()
+    val kMeansModel = KMeans.train(vectors, 12, 40)
+    val predictions = latLong.map{ll => (kMeansModel.predict(Vectors.dense(ll._1, ll._2)),1)}
+
+    val mostDagerousZones = predictions.reduceByKey(_+_)
+      .sortBy(-_._2)
+      .take(3)
+
+    val lessDagerousZones = predictions.reduceByKey(_+_)
+      .sortBy(_._2)
+      .take(3)
+
+    sc.parallelize(mostDagerousZones).saveAsTextFile("M2SID-ProjetBigData-Sbt\\Q3-MostDangerousZones")
+    sc.parallelize(lessDagerousZones).saveAsTextFile("M2SID-ProjetBigData-Sbt\\Q3-LessDangerousZones")
+
+  }
+
+}
